6f12109e74d9e6721e3e6dd04ab995ddc573f584
salamint <salamint@proton.me>
2025-07-25 20:06:41 +0200

    utils: Created a utils package

---
+++ src/blasmodcli/utils/__init__.py
@@ -0,0 +1,5 @@
+from .colors import Color
+from .directories import Directories
+from .io import Counter, Message, Progress, Table
+
+MODDING_INSTALLER_REPOSITORY = "https://github.com/BrandenEK/Blasphemous.Modding.Installer"
