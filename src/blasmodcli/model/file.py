a77aa0982d48949523f3ccd902b4b6242acf461b
salamint <salamint@proton.me>
2025-12-26 21:03:12 +0100

    model: Added the ModInstallation and File classes to the model package

---
+++ src/blasmodcli/model/file.py
@@ -0,0 +1,15 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from blasmodcli.model.base import Base
+from blasmodcli.model.mod_installation import ModInstallation
+
+
+class File(Base):
+    __tablename__ = "file"
+
+    directory: Mapped[str] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(primary_key=True)
+
+    mod_name: Mapped[str] = mapped_column(ForeignKey(ModInstallation.mod_name))
+    mod: Mapped[ModInstallation] = relationship(ModInstallation, back_populates="files")
