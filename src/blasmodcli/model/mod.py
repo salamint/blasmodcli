312fbc38ddd47fb50f47e0e1a1d08256356f51ca
salamint <salamint@proton.me>
2025-12-27 16:55:23 +0100

    model: Added a class method to build a Mod from metadata taken from a JSON

--- src/blasmodcli/model/mod.py
+++ src/blasmodcli/model/mod.py
@@ -1,5 +1,7 @@
-from datetime import date
+from datetime import date, datetime
+from enum import IntEnum
 from typing import List, Self
+from requests import get as request
 
 from sqlalchemy import ForeignKey, UniqueConstraint
 from sqlalchemy.orm import Mapped, relationship
@@ -11,6 +13,20 @@ from blasmodcli.model.dependency import Dependency
 from blasmodcli.model.game import Game
 from blasmodcli.model.mod_source import ModSource
 from blasmodcli.model.version import Version
+
+
+AUTHORS_SEPARATOR = " && "
+
+
+class DateFormat:
+    SIMPLE = "%Y-%m-%d"
+    DETAILED = "%A, %e %B %Y"
+
+
+class ModState(IntEnum):
+    NONE = 0
+    INSTALLED = 1
+    ACTIVATED = 2
 
 
 class Mod(Base):
@@ -36,7 +52,32 @@ class Mod(Base):
 
     plugin_file_name: Mapped[str]
 
-    dependencies: Mapped[List[Self]] = relationship(Dependency, back_populates="mod_name")
-    required_by: Mapped[List[Self]] = relationship(Dependency, back_populates="dependency_name")
+    dependencies: Mapped[List[Self]] = relationship(Dependency, back_populates="mod")
+    required_by: Mapped[List[Self]] = relationship(Dependency, back_populates="dependency")
 
     authors: Mapped[List['Authorship']] = relationship("Authorship", back_populates="mod")
+
+    @classmethod
+    def from_raw_data(cls, source: 'ModSource', data: dict):
+        repository = f"https://github.com/{data['GithubAuthor']}/{data['GithubRepo']}"
+        response = request(f"{repository}/releases/latest")
+        response.raise_for_status()
+        version = Version.from_string(response.url.split("/")[-1])
+
+        mod = cls(
+            game_name=source.game.name,
+            source_name=source.name,
+            name=data["Name"],
+            description=data["Description"],
+            release_date=datetime.strptime(data["InitialReleaseDate"], DateFormat.SIMPLE).date(),
+            repository=repository,
+            plugin_file_name=data["PluginFile"],
+        )
+
+        authors = data["Author"].replace(", ", AUTHORS_SEPARATOR).replace(", && ", AUTHORS_SEPARATOR)
+        for author in authors.split(AUTHORS_SEPARATOR):
+            mod.authors.append(author.strip())
+
+        for dependency in data.get("Dependencies", []):
+            mod.dependencies.append(dependency)
+        return mod
