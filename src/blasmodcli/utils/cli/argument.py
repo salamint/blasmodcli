9ddb7ee4edadb46ac4894d9d90d5650d961ca4e2
salamint <salamint@proton.me>
2026-01-04 19:37:35 +0100

    cli: Added support of annotations to Argument objects

--- src/blasmodcli/utils/cli/argument.py
+++ src/blasmodcli/utils/cli/argument.py
@@ -4,16 +4,33 @@ from typing import Any
 
 class Argument:
 
-    def __init__(self, *names: str, default: Any = None, type: type = None, help: str = None):
+    def __init__(self, *names: str, action: str = None, default: Any = None, type: type = None, help: str = None):
+        self.action = action
         self.names = list(names)
         self.default = default
         self.type = type
         self.help = help
 
+    def add_annotation(self, name: str, type_: type):
+        self.names.insert(0, "--" + name.replace("_", "-"))
+        if self.type is None:
+            self.type = type_
+
+    def get_action(self) -> str | None:
+        if self.action:
+            return self.action
+
+        if self.type is bool:
+            if self.default is False:
+                return "store_true"
+            else:
+                return "store_false"
+        return None
+
     def add_argument_to(self, parser: ArgumentParser):
         parser.add_argument(
             *self.names,
+            action=self.get_action(),
             default=self.default,
-            type=self.type,
             help=self.help
         )
--- src/blasmodcli/utils/cli/command.py
+++ src/blasmodcli/utils/cli/command.py
@@ -8,18 +8,19 @@ class MetaCommandHandler(ABCMeta):
 
     def __init__(cls, name: str, bases: tuple[type], dct: dict[str, Any]):
         super().__init__(name, bases, dct)
-        cls.arguments: list['Argument'] = []
-        for attr in dct.values():
+        cls.arguments: dict[str, 'Argument'] = {}
+
+        for attr, value in dct.items():
             if isinstance(attr, Argument):
-                cls.arguments.append(attr)
-        for k, v in cls.__annotations__.items():
-            arg = dct[k]
-            arg.names.insert(0, "--" + k.replace("_", "-"))
-            arg.type = v
+                cls.arguments[name] = value
+
+        for arg_name, arg_type in cls.__annotations__.items():
+            arg = cls.arguments[arg_name]
+            arg.add_annotation(arg_name, arg_type)
 
     def add_subparser_to(cls, subparsers) -> None:
         subparser = subparsers.add_parser(cls.__name__.lower(), help=cls.__doc__)
-        for arg in cls.arguments:
+        for arg in cls.arguments.values():
             arg.add_argument_to(subparser)
 
 
