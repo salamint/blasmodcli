6afb5349bd0eea71953b17b625e83a3a1bb58f43
salamint <salamint@proton.me>
2026-01-06 00:16:32 +0100

    controller: Added a group for commands that operate on a game

---
+++ src/blasmodcli/controller/game/group.py
@@ -0,0 +1,26 @@
+from abc import ABC
+
+from blasmodcli.model import ModState, ModVersion
+from blasmodcli.utils import Message
+from blasmodcli.utils.cli import CommandHandler
+
+
+class GameCommandGroup(CommandHandler, ABC):
+    """ Regroups commands that operate on a single mod. """
+    __group__ = "game"
+
+    def get_mods(self, state: ModState = ModState.NONE) -> list[ModVersion]:
+        precision = f" that are {state.name.lower()}" if state is not ModState.NONE else ""
+        Message.debug(f"Querying all mods for {self.game.title}{precision}...")
+        mod_versions = []
+        match state:
+            case ModState.NONE:
+                for mod in self.game.mods:
+                    mod_versions.append(ModVersion(mod))
+            case ModState.CACHED:
+                for mod_version in self.fs.cache.get_all_latest_versions(self.game):
+                    mod_versions.append(mod_version)
+            case ModState.INSTALLED:
+                for mod_version in self.fs.installations.get_all_latest_versions(self.game):
+                    mod_versions.append(mod_version)
+        return mod_versions
