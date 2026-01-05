14fa10cd85abdc49e8ca0e9f6ede66f3184f574e
salamint <salamint@proton.me>
2026-01-06 00:14:24 +0100

    controller: Created the 'mod' controller package

---
+++ src/blasmodcli/controller/mod/__init__.py
@@ -0,0 +1,6 @@
+from .activate import Activate
+from .deactivate import Deactivate
+from .info import Info
+from .install import Install
+from .uninstall import Uninstall
+from .upgrade import Upgrade
