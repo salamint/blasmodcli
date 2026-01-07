9d544eafaaafe6024dc8e0cb17e7e3608301bff5
salamint <salamint@proton.me>
2026-01-07 15:03:39 +0100

    controller: Added a controller for the 'search' command

---
+++ src/blasmodcli/controller/game/search.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+from blasmodcli.controller.game.group import GameCommandGroup
+from blasmodcli.utils import Message
+from blasmodcli.utils.cli import Argument
+from blasmodcli.view import Formatter
+
+WILDCARD = "%"
+
+
+class Search(GameCommandGroup):
+    """ Lists every mod whose name, author or description contains the given string of text. """
+
+    source: Optional[str] = Argument("-s", default=None, help="The name of the source in which you want to search.")
+    terms: list[str] = Argument(nargs="*", help="The list of terms that must appear in the name or description of the mod.")
+
+    async def handle(self) -> int:
+        pattern = WILDCARD + WILDCARD.join(self.terms) + WILDCARD
+        Message.debug(f"Searching for mod names or descriptions matching: {pattern}")
+        formatter = Formatter(self.fs)
+        for mod in self.tables.mods.search(self.game, self.source, pattern):
+            formatter.summary(mod)
+        return 0
