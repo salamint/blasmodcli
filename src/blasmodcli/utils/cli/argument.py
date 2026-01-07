63f0097971426ce6234c86a08f0dd4bba44a6ade
salamint <salamint@proton.me>
2026-01-07 15:16:51 +0100

    cli: Arguments are loaded from the command's class atribute and added to the argument parser

--- src/blasmodcli/utils/cli/argument.py
+++ src/blasmodcli/utils/cli/argument.py
@@ -1,9 +1,19 @@
+from argparse import ArgumentParser
+from typing import Any
 
 
 class Argument:
 
-    def __init__(self, *names: str, default, type: type, help: str):
-        self.names = names
+    def __init__(self, *names: str, default: Any = None, type: type = None, help: str = None):
+        self.names = list(names)
         self.default = default
         self.type = type
         self.help = help
+
+    def add_argument_to(self, parser: ArgumentParser):
+        parser.add_argument(
+            *self.names,
+            default=self.default,
+            type=self.type,
+            help=self.help
+        )
--- src/blasmodcli/utils/cli/command.py
+++ src/blasmodcli/utils/cli/command.py
@@ -1,20 +1,26 @@
 from abc import ABCMeta, ABC, abstractmethod
 from typing import Any
 
+from .argument import Argument
+
 
 class MetaCommandHandler(ABCMeta):
 
     def __init__(cls, name: str, bases: tuple[type], dct: dict[str, Any]):
         super().__init__(name, bases, dct)
         cls.arguments: list['Argument'] = []
-        print(name)
+        for attr in dct.values():
+            if isinstance(attr, Argument):
+                cls.arguments.append(attr)
         for k, v in cls.__annotations__.items():
-            print(k, v)
+            arg = dct[k]
+            arg.names.insert(0, "--" + k.replace("_", "-"))
+            arg.type = v
 
     def add_subparser_to(cls, subparsers) -> None:
         subparser = subparsers.add_parser(cls.__name__.lower(), help=cls.__doc__)
         for arg in cls.arguments:
-            subparser.add_argument(*arg.names)
+            arg.add_argument_to(subparser)
 
 
 class CommandHandler(ABC, metaclass=MetaCommandHandler):
