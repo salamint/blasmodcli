507d862dadeed104c6812afc227c83af54066232
salamint <salamint@proton.me>
2026-01-08 20:27:15 +0100

    model: Fixed the ambiguous foreign keys between the mod and dependency tables, and the dependency and version tables

--- src/blasmodcli/model/dependency.py
+++ src/blasmodcli/model/dependency.py
@@ -9,16 +9,16 @@ class Dependency(Base):
     __tablename__ = "dependency"
 
     mod_id: Mapped[int] = mapped_column(ForeignKey("mod.id"), primary_key=True)
-    mod: Mapped['Mod'] = relationship("Mod", back_populates="dependencies")
+    mod: Mapped['Mod'] = relationship("Mod", back_populates="dependencies", foreign_keys=[mod_id])
 
     dependency_id: Mapped[int] = mapped_column(ForeignKey("mod.id"), primary_key=True)
-    dependency: Mapped['Mod'] = relationship("Mod", back_populates="required_by")
+    dependency: Mapped['Mod'] = relationship("Mod", back_populates="required_by", foreign_keys=[dependency_id])
 
     minimum_version_id: Mapped[int | None] = mapped_column(ForeignKey("version.id"), default=None)
-    minimum_version: Mapped['Version'] = relationship("Version")
+    minimum_version: Mapped['Version'] = relationship("Version", foreign_keys=[minimum_version_id])
 
     maximum_version_id: Mapped[int | None] = mapped_column(ForeignKey("version.id"), default=None)
-    maximum_version: Mapped['Version'] = relationship("Version")
+    maximum_version: Mapped['Version'] = relationship("Version", foreign_keys=[maximum_version_id])
 
 
 from blasmodcli.model.mod import Mod
