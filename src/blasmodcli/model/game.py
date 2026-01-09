ffc03a1023c7e3b11d98ccda4440d7e8719a3328
salamint <salamint@proton.me>
2026-01-09 15:36:29 +0100

    model: The primary key of the game table is now the game's name instead of an integer

--- src/blasmodcli/model/game.py
+++ src/blasmodcli/model/game.py
@@ -1,19 +1,20 @@
+from pathlib import Path
 from typing import List
 
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from blasmodcli.model.base import Base
+from blasmodcli.model.path import PathType
 
 
 class Game(Base):
     __tablename__ = "game"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(unique=True)
+    name: Mapped[str] = mapped_column(primary_key=True)
     mod_loader: Mapped[str]
     modding_tools_url: Mapped[str]
     linux_native: Mapped[bool]
-    saves_directory: Mapped[str]
+    saves_directory: Mapped[Path] = mapped_column(PathType)
 
     sources: Mapped[List['ModSource']] = relationship("ModSource", back_populates="game")
 
