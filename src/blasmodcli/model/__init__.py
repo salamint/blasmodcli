bbd8d50090db2841d669a382b8d721e629e73a72
salamint <salamint@proton.me>
2026-01-10 15:23:43 +0100

    model: Importing ModState in 'model' package

--- src/blasmodcli/model/__init__.py
+++ src/blasmodcli/model/__init__.py
@@ -3,7 +3,7 @@ from .authorship import Authorship
 from .dependency import Dependency
 from .file import File
 from .game import Game
-from .mod import Mod
+from .mod import Mod, ModState
 from .mod_installation import ModInstallation
 from .mod_source import ModSource
 from .version import Version
