9412759dcbe11ca33fe32c5f62bb369660fd76a9
salamint <salamint@proton.me>
2026-01-10 22:45:15 +0100

    repositories: Added a new repository to manage mod sources

---
+++ src/blasmodcli/repositories/mod_source.py
@@ -0,0 +1,24 @@
+from blasmodcli.model import ModSource, Game
+from blasmodcli.repositories.repository import Repository
+
+
+class ModSourceRepository(Repository):
+
+    def get_all_by_game(self, game: Game) -> list[type[ModSource]]:
+        with self.session() as session:
+            return session.query(ModSource).filter_by(game_name=game.name).all()
+
+    def update(self, sources: list[ModSource]):
+        with self.session() as session:
+            for source in sources:
+                query = session.query(ModSource).filter_by(game_name=source.game.name, name=source.name)
+                in_db = query.one_or_none()
+                if in_db is not None:
+                    query.update({
+                        "format": source.format,
+                        "url": source.url,
+                        "maintainer": source.maintainer,
+                    })
+                else:
+                    session.add(source)
+            session.commit()
