f3d9a2e7beecb2f11e2b60231e47e20e17d4746b
salamint <salamint@proton.me>
2026-01-10 12:47:07 +0100

    utils: Split the 'utils.io' module into multiple submodules inside an 'io' package

---
+++ src/blasmodcli/utils/io/table.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass
+
+from blasmodcli.utils import Color
+
+
+@dataclass
+class TableRow:
+    header: str
+    value: str
+    color: Color | None
+
+    def print(self, row_size: int):
+        formatted_header = Color.fmt(self.header, Color.WHITE)
+        if len(self.header) < row_size:
+            formatted_header += " " * (row_size - len(self.header))
+        formatted_value = self.value if self.color is None else Color.fmt(self.value, self.color)
+        print(formatted_header, formatted_value)
+
+
+class Table:
+
+    def __init__(self, row_size: int = 0):
+        self.row_size = row_size
+        self.dynamic = row_size <= 0
+        self.rows: list[TableRow] = []
+
+    def add_row(self, header: str, value, color: Color | None = None):
+        header_size = len(header)
+        if self.dynamic and self.row_size < header_size:
+            self.row_size = header_size
+        self.rows.append(TableRow(header, value, color))
+
+    def print(self):
+        for row in self.rows:
+            row.print(self.row_size)
