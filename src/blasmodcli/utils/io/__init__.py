1b12fa314ed5fd04e5497295cb149aa3c078515f
salamint <salamint@proton.me>
2026-01-10 12:47:07 +0100

    utils: Split the 'utils.io' module into multiple submodules inside an 'io' package

---
+++ src/blasmodcli/utils/io/__init__.py
@@ -0,0 +1,4 @@
+from .counter import Counter
+from .message import Message
+from .progress import Progress
+from .table import Table
---
+++ src/blasmodcli/utils/io/progress.py
@@ -0,0 +1,32 @@
+from blasmodcli.model import Version
+from blasmodcli.utils import Color
+
+
+class Progress:
+
+    def __init__(self):
+        self.__status = None
+
+    def status(self, status: str, color: 'Color'):
+        if self.__status is not None:
+            return
+        print(f" {Color.fmt(status, color)}", flush=True)
+        self.__status = status
+
+    def bool(self, boolean: bool):
+        if boolean:
+            self.success()
+        else:
+            self.failure()
+
+    def failure(self):
+        self.status("FAILURE", Color.RED)
+
+    def success(self):
+        self.status("SUCCESS", Color.GREEN)
+
+    def version(self, version: 'Version'):
+        self.status(str(version), Color.YELLOW)
+
+    def has_succeeded(self) -> bool:
+        return self.__status is not None and self.__status
