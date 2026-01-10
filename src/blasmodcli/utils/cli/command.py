67fdd9e7fda531013b56892c7c4b57a35154f9ee
salamint <salamint@proton.me>
2026-01-10 20:44:41 +0100

    cli: Improved the readability of the MetaCommandHandler class

--- src/blasmodcli/utils/cli/command.py
+++ src/blasmodcli/utils/cli/command.py
@@ -1,77 +1,70 @@
-import sys
-import traceback
 from abc import ABCMeta, ABC, abstractmethod
 from argparse import Namespace
-from typing import Any
+from typing import Any, Self
 
-from blasmodcli.exceptions import DoneException, CancelException
-from blasmodcli.model.game import Game
+from blasmodcli.exceptions import NothingToDoException, UserCancelException, CommandInMultipleGroupsError
+from blasmodcli.model import Game
 from blasmodcli.repositories import Warehouse
-from blasmodcli.utils import Color
 from blasmodcli.utils.cli import Argument, Choices
 from blasmodcli.view import Message
 
-_command_groups: dict[str, 'MetaCommandHandler'] = {}
-
-
-class CommandLogicError(Exception):
-
-    @classmethod
-    def multiple_groups(cls, command: str, base_groups: tuple[type]):
-        number_of_groups = 0
-        for group in base_groups:
-            if group in _command_groups.keys():
-                number_of_groups += 1
-        return f"The '{command}' command cannot be part of {number_of_groups} groups. it can only be in one group."
+Attributes = dict[str, Any]
 
 
 class MetaCommandHandler(ABCMeta):
 
-    def __new__(metacls, name: str, bases: tuple[type], dct: dict[str, Any]):
+    _command_groups: dict[str, 'MetaCommandHandler'] = {}
+
+    @classmethod
+    def get_group(metacls, name: str, bases: tuple[type]) -> Self | None:
+        in_group = None
+        for group_name, group in metacls._command_groups.items():
+            if group_name not in bases:
+                continue
+            if in_group is not None:
+                group_names = list(metacls._command_groups.keys())
+                raise CommandInMultipleGroupsError(group_names, name, bases)
+            in_group = group
+        return in_group
+
+    def __new__(metacls, name: str, bases: tuple[type], dct: Attributes):
         if "__group__" in dct.keys():
             group_instance = super().__new__(metacls, name, bases, dct)
-            _command_groups[name] = group_instance
+            metacls._command_groups[name] = group_instance
             return group_instance
 
-        group = None
-        for base in bases:
-            group_match = _command_groups.get(base.__name__)
-            if group_match is not None:
-                if group is not None:
-                    return CommandLogicError.multiple_groups(name, bases)
-                group = group_match
+        group = metacls.get_group(name, bases)
+        if group is not None:
+            inherit_from_group(dct, group)
 
-        if group:
-            grp_annotations = group.__annotations__.copy()
-            grp_annotations.update(dct.get("__annotations__", {}))
-            dct["__annotations__"] = grp_annotations
-
-            for arg_name, arg_value in group.arguments.items():
-                if arg_name not in dct.keys():
-                    dct[arg_name] = arg_value.copy()
         instance = super().__new__(metacls, name, bases, dct)
         return instance
 
     def __init__(cls, name: str, bases: tuple[type], dct: dict[str, Any]):
         super().__init__(name, bases, dct)
         cls.arguments: dict[str, 'Argument'] = {}
+        cls.choices: dict[str, 'Choices'] = {}
 
         for attr, value in dct.items():
             if isinstance(value, Argument):
                 cls.arguments[attr] = value
+            elif isinstance(value, Choices):
+                cls.choices[attr] = value
 
-        for arg_name, arg_type in cls.__annotations__.items():
-            arg = cls.arguments.get(arg_name)
-            if arg is None:
-                choices = cls.__dict__.get(arg_name)
-                if isinstance(choices, Choices):
-                    choices.destination = arg_name
-            else:
-                arg.add_annotation(arg_name, arg_type)
+        cls.add_annotations()
 
     @property
     def command(cls):
         return cls.__name__.lower()
+
+    def add_annotations(cls):
+        for arg_name, arg_type in cls.__annotations__.items():
+            arg = cls.arguments.get(arg_name)
+            choices = cls.choices.get(arg_name)
+            if arg is not None:
+                arg.add_annotation(arg_name, arg_type)
+            elif choices is not None:
+                choices.destination = arg_name
 
     def add_subparser_to(cls, subparsers) -> None:
         subparser = subparsers.add_parser(cls.command, help=cls.__doc__)
@@ -83,16 +76,25 @@ class MetaCommandHandler(ABCMeta):
 
         try:
             return instance.handle()
-        except DoneException as e:
+        except NothingToDoException as e:
             Message.success(str(e))
             return 0
-        except CancelException as e:
+        except UserCancelException as e:
             Message.error(str(e))
             return 0
         except Exception as e:
             Message.error(f"{e.__class__.__name__}: {e}")
-            Message.print(Color.RED, traceback.format_exc(), stream=sys.stderr)
-            return 1
+            raise e
+
+
+def inherit_from_group(dct: Attributes, group: MetaCommandHandler):
+    grp_annotations = group.__annotations__.copy()
+    grp_annotations.update(dct.get("__annotations__", {}))
+    dct["__annotations__"] = grp_annotations
+
+    for arg_name, arg_value in group.arguments.items():
+        if arg_name not in dct.keys():
+            dct[arg_name] = arg_value.copy()
 
 
 class CommandHandler(ABC, metaclass=MetaCommandHandler):
