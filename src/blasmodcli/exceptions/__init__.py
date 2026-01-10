b0189ca58aef1d61936ec5c7c45cdd78e57f407b
salamint <salamint@proton.me>
2026-01-10 20:19:02 +0100

    exceptions: Created an 'exceptions' package

---
+++ src/blasmodcli/exceptions/__init__.py
@@ -0,0 +1,4 @@
+from .argument import ArgumentValueError, ModNameError, UnknownModError, MultipleModsError
+from .backend import CriticalBackendException, CommandInMultipleGroupsError
+from .base import ApplicationException
+from .controller import ControllerException, NothingToDoException, UserCancelException
