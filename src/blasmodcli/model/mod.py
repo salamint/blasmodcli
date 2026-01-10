3f42498636f75a492c283d8c6d04729f610fbf49
salamint <salamint@proton.me>
2026-01-10 18:18:26 +0100

    model: Removed the 'from_raw_data' method from the Mod class

--- src/blasmodcli/model/mod.py
+++ src/blasmodcli/model/mod.py
@@ -1,7 +1,6 @@
-from datetime import date, datetime
+from datetime import date
 from enum import IntEnum
 from typing import List
-from requests import get as request
 
 from sqlalchemy import ForeignKey, UniqueConstraint
 from sqlalchemy.orm import Mapped, relationship
@@ -9,8 +8,6 @@ from sqlalchemy.orm import mapped_column
 
 from blasmodcli.model.base import Base
 from blasmodcli.model.version import Version, VersionType
-
-AUTHORS_SEPARATOR = " && "
 
 
 class DateFormat:
@@ -62,31 +59,6 @@ class Mod(Base):
     )
 
     authors: Mapped[List['Authorship']] = relationship("Authorship", back_populates="mod")
-
-    @classmethod
-    def from_raw_data(cls, source: 'ModSource', data: dict):
-        repository = f"https://github.com/{data['GithubAuthor']}/{data['GithubRepo']}"
-        response = request(f"{repository}/releases/latest")
-        response.raise_for_status()
-        version = Version.from_string(response.url.split("/")[-1])
-
-        mod = cls(
-            game_name=source.game.name,
-            source_name=source.name,
-            name=data["Name"],
-            description=data["Description"],
-            release_date=datetime.strptime(data["InitialReleaseDate"], DateFormat.SIMPLE).date(),
-            repository=repository,
-            plugin_file_name=data["PluginFile"],
-        )
-
-        authors = data["Author"].replace(", ", AUTHORS_SEPARATOR).replace(", && ", AUTHORS_SEPARATOR)
-        for author in authors.split(AUTHORS_SEPARATOR):
-            mod.authors.append(author.strip())
-
-        for dependency in data.get("Dependencies", []):
-            mod.dependencies.append(dependency)
-        return mod
 
 
 from blasmodcli.model.authorship import Authorship
