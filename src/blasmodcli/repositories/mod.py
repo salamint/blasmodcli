e34cc0bed42ac7fc6e705c1263c9bec08a460433
salamint <salamint@proton.me>
2026-01-17 08:45:48 +0100

    repositories: Filtered using filter instead of filter_by, to more efficiently rename attributes and/or find errors in queries

--- src/blasmodcli/repositories/mod.py
+++ src/blasmodcli/repositories/mod.py
@@ -15,27 +15,46 @@ class ModRepository(Repository):
 
     def get_all_by_name(self, game: Game, name: str) -> list[type[Mod]]:
         with self.session() as session:
-            return session.query(Mod).filter_by(
-                game_id=game.id,
-                name=name
+            return session.query(Mod).filter(
+                Mod.game_id == game.id,
+                Mod.name == name
             ).all()
 
     def get_by_name(self, source: ModSource, name: str) -> type[Mod]:
         with self.session() as session:
-            return session.query(Mod).filter_by(
-                game_id=source.game_id,
-                source_name=source.name,
-                name=name
+            return session.query(Mod).filter(
+                Mod.game_id == source.game_id,
+                Mod.source_name == source.name,
+                Mod.name == name
             ).one()
 
     def search(self, game: Game, source: Optional[str], pattern: str) -> list[type[Mod]]:
         with self.session() as session:
-            query = session.query(Mod).filter_by(
-                game_id=game.id
+            query = session.query(Mod).filter(
+                Mod.game_id == game.id
             ).filter(or_(
                 Mod.name.ilike(pattern),
                 Mod.description.ilike(pattern)
             ))
             if source is not None:
-                query = query.filter_by(sourc_name=source)
+                query = query.filter(Mod.source_name == source)
             return query.all()
+
+    def update_all(self, mods: list[Mod]):
+        for mod in mods:
+            self.update(mod)
+
+    def update(self, mod: Mod):
+        with self.session() as session:
+            query = session.query(Mod).filter(
+                Mod.game_id == mod.game_id,
+                Mod.source_name == mod.source_name,
+                Mod.name == mod.name
+            )
+            in_db = query.one_or_none()
+            if in_db is not None:
+                # TODO: dependencies
+                query.update({})
+            else:
+                session.add(mod)
+            session.commit()
--- src/blasmodcli/repositories/mod_source.py
+++ src/blasmodcli/repositories/mod_source.py
@@ -6,12 +6,15 @@ class ModSourceRepository(Repository):
 
     def get_all_by_game(self, game: Game) -> list[type[ModSource]]:
         with self.session() as session:
-            return session.query(ModSource).filter_by(game_id=game.id).all()
+            return session.query(ModSource).filter(ModSource.game_id == game.id).all()
 
     def update(self, sources: list[ModSource]):
         with self.session() as session:
             for source in sources:
-                query = session.query(ModSource).filter_by(game_id=source.game.id, name=source.name)
+                query = session.query(ModSource).filter(
+                    ModSource.game_id == source.game.id,
+                    ModSource.name == source.name
+                )
                 in_db = query.one_or_none()
                 if in_db is not None:
                     query.update({
@@ -22,3 +25,13 @@ class ModSourceRepository(Repository):
                 else:
                     session.add(source)
             session.commit()
+
+    def update(self, source: ModSource):
+        query = self.session.query(ModSource).filter(
+            ModSource.game_id == source.game_id,
+            ModSource.name == source.name
+        )
+        in_db = query.one_or_none()
+        if in_db is None:
+            self.session.add(source)
+        self.session.commit()
