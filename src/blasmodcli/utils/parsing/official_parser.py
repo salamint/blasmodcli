6426088dca3212c17dd1cb2545fc480eda9444b7
salamint <salamint@proton.me>
2026-01-19 20:40:21 +0100

    parsing: Rewrote the mod list parsers to use the asynchronous aiohttp package instead of requests

--- src/blasmodcli/utils/parsing/official_parser.py
+++ src/blasmodcli/utils/parsing/official_parser.py
@@ -1,9 +1,11 @@
+import json
 from datetime import datetime
-from requests import get as request
+
+from aiohttp import ClientSession
 from typing import Any, Dict, Generator
 
 from blasmodcli.model import Authorship, Mod, ModSource
-from blasmodcli.utils.parsing.parser import ModListParser, ModParser
+from blasmodcli.utils.parsing.parser import ModListParser
 from blasmodcli.view import DateFormat
 
 Object = Dict[str, Any]
@@ -11,10 +13,11 @@ Object = Dict[str, Any]
 AUTHORS_SEPARATOR = " && "
 
 
-def fetch_latest_version(repository: str) -> str:
-    response = request(f"{repository}/releases/latest")
-    response.raise_for_status()
-    return response.url.split("/")[-1]
+async def fetch_latest_version(session: ClientSession, repository: str) -> str:
+    url = f"{repository}/releases/latest"
+    async with session.get(url) as response:
+        response.raise_for_status()
+        return response.url.path
 
 
 def parse_authors(string: str) -> Generator[str]:
@@ -23,60 +26,46 @@ def parse_authors(string: str) -> Generator[str]:
         yield name.strip()
 
 
-class OfficialModParser(ModParser):
-    __parser_name__ = "official"
-
-    def __init__(self, list_parser: 'ModListParser', data: Object):
-        super().__init__(list_parser)
-        self.data = data
-
-    def get_name(self) -> str:
-        return self.data["Name"]
-
-    def parse_mod(self) -> Mod:
-        repository = f"https://github.com/{self.data['GithubAuthor']}/{self.data['GithubRepo']}"
-        mod = Mod(
-            game_id=self.list_parser.source.game.id,
-            source_name=self.list_parser.source.name,
-            name=self.get_name(),
-            description=self.data["Description"],
-            release_date=datetime.strptime(self.data["InitialReleaseDate"], DateFormat.SIMPLE).date(),
-            repository=repository,
-            plugin_file_name=self.data["PluginFile"],
-            version=fetch_latest_version(repository)
-        )
-        for author in parse_authors(self.data["Author"]):
-            mod.authors.append(Authorship(mod=mod, name=author))
-        return mod
-
-    def parse_dependencies(self) -> list[str]:
-        return self.data.get("Dependencies", [])
-
-
 class OfficialModListParser(ModListParser):
+    __parser_name__ = "official"
 
     def __init__(self, source: ModSource):
         super().__init__(source)
-        self.index = 0
         self.data: list[Object] = []
 
-    def __next__(self) -> 'ModParser':
-        if self.index < self.count():
-            data = self.data[self.index]
-            if not isinstance(data, dict):
-                raise TypeError(f"The value at index {self.index} is of type {type(data)} and not an object.")
-            self.index += 1
-            return OfficialModParser(self, data)
-        raise StopIteration
-
-    def count(self) -> int:
-        return len(self.data)
-
-    def fetch(self):
-        response = request(self.source.url)
-        response.raise_for_status()
-        data = response.json()
-
+    async def fetch(self):
+        async with ClientSession() as session:
+            async with session.get(self.source.url) as response:
+                response.raise_for_status()
+                content = await response.content.read()
+        data = json.loads(content)
         if not isinstance(data, list):
             raise TypeError(f"The JSON file's contents should be a list of objects, got '{type(self.data)}' instead.")
         self.data = data
+        self.total = len(self.data)
+
+    async def parse(self):
+        async with ClientSession() as session:
+            await self.parse_all_mods(session)
+
+    async def parse_all_mods(self, session: ClientSession):
+        for i, data in enumerate(self.data):
+            if not isinstance(data, dict):
+                raise TypeError(f"The value at index {i} is of type {type(data)} and not an object.")
+            await self.parse_mod(session, data)
+
+    async def parse_mod(self, session: ClientSession, data: Object):
+        repository = f"https://github.com/{data['GithubAuthor']}/{data['GithubRepo']}"
+        mod = Mod(
+            game_id=self.source.game.id,
+            source_name=self.source.name,
+            name=data["Name"],
+            description=data["Description"],
+            release_date=datetime.strptime(data["InitialReleaseDate"], DateFormat.SIMPLE).date(),
+            repository=repository,
+            plugin_file_name=data["PluginFile"],
+            version=fetch_latest_version(session, repository)
+        )
+        for author in parse_authors(data["Author"]):
+            mod.authors.append(Authorship(mod=mod, name=author))
+        return mod
