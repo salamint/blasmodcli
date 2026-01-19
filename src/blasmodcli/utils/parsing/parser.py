519b5c520d37833543d4cf862c3f18a97c6d581f
salamint <salamint@proton.me>
2026-01-19 20:40:21 +0100

    parsing: Rewrote the mod list parsers to use the asynchronous aiohttp package instead of requests

--- src/blasmodcli/utils/parsing/parser.py
+++ src/blasmodcli/utils/parsing/parser.py
@@ -1,32 +1,7 @@
 from abc import ABC, abstractmethod
-from typing import Iterator
 
 from blasmodcli.model import Mod, ModSource, Dependency
 from blasmodcli.utils.parsing.meta_parser import MetaModListParser
-
-
-class ModParser(ABC):
-
-    def __init__(self, list_parser: 'ModListParser'):
-        self.list_parser = list_parser
-
-    @abstractmethod
-    def get_name(self) -> str:
-        pass
-
-    @abstractmethod
-    def parse_mod(self) -> Mod:
-        pass
-
-    @abstractmethod
-    def parse_dependencies(self) -> list[str]:
-        pass
-
-    def parse(self) -> Mod:
-        mod = self.parse_mod()
-        self.list_parser.mods[mod.name] = mod
-        self.list_parser.dependencies[mod.name] = self.parse_dependencies()
-        return mod
 
 
 class ModListParser(ABC, metaclass=MetaModListParser):
@@ -35,26 +10,22 @@ class ModListParser(ABC, metaclass=MetaModListParser):
         self.source = source
         self.mods: dict[str, Mod] = {}
         self.dependencies: dict[str, list[str]] = {}
+        self.done = 0
+        self.total = 0
 
-    def __iter__(self) -> Iterator[ModParser]:
-        return self
-
-    def __len__(self) -> int:
-        return self.count()
+    def extend(self, mods: list[Mod]):
+        for mod in self.mods.values():
+            mods.append(mod)
 
     @abstractmethod
-    def __next__(self) -> 'ModParser':
+    async def fetch(self):
         pass
 
     @abstractmethod
-    def count(self) -> int:
+    async def parse(self):
         pass
 
-    @abstractmethod
-    def fetch(self):
-        pass
-
-    def add_dependencies(self):
+    def resolve_dependencies(self):
         for mod_name, dependencies in self.dependencies.items():
             mod = self.mods[mod_name]
             for dependency_name in dependencies:
