6ac005b45d3da7a359d9c29bac5e684b40b0a9f3
salamint <salamint@proton.me>
2026-01-20 11:21:51 +0100

    parsing: Added some abstract methods to allow the parsing of mods one by one

--- src/blasmodcli/utils/parsing/official_parser.py
+++ src/blasmodcli/utils/parsing/official_parser.py
@@ -2,13 +2,11 @@ import json
 from datetime import datetime
 
 from aiohttp import ClientSession
-from typing import Any, Dict, Generator
+from typing import Generator
 
 from blasmodcli.model import Authorship, Mod, ModSource
-from blasmodcli.utils.parsing.parser import ModListParser
+from blasmodcli.utils.parsing.parser import ModListParser, Object
 from blasmodcli.view import DateFormat
-
-Object = Dict[str, Any]
 
 AUTHORS_SEPARATOR = " && "
 
@@ -31,7 +29,7 @@ class OfficialModListParser(ModListParser):
 
     def __init__(self, source: ModSource):
         super().__init__(source)
-        self.data: list[Object] = []
+        self.all_data: list[Object] = []
 
     async def fetch(self):
         async with ClientSession() as session:
@@ -40,31 +38,29 @@ class OfficialModListParser(ModListParser):
                 content = await response.content.read()
         data = json.loads(content)
         if not isinstance(data, list):
-            raise TypeError(f"The JSON file's contents should be a list of objects, got '{type(self.data)}' instead.")
-        self.data = data
-        self.total = len(self.data)
+            raise TypeError(f"The JSON file's contents should be a list of objects, got '{type(self.all_data)}' instead.")
+        self.all_data = data
+        self.total = len(self.all_data)
 
-    async def parse(self):
-        async with ClientSession() as session:
-            await self.parse_all_mods(session)
-
-    async def parse_all_mods(self, session: ClientSession):
-        for i, data in enumerate(self.data):
+    def data(self) -> Generator[Object]:
+        for i, data in enumerate(self.all_data):
             if not isinstance(data, dict):
                 raise TypeError(f"The value at index {i} is of type {type(data)} and not an object.")
-            await self.parse_mod(session, data)
+            yield data
 
-    async def parse_mod(self, session: ClientSession, data: Object):
+    async def parse(self, data: Object) -> Mod:
         repository = f"https://github.com/{data['GithubAuthor']}/{data['GithubRepo']}"
+        async with ClientSession() as session:
+            version = await fetch_latest_version(session, repository)
         mod = Mod(
-            game_id=self.source.game.id,
+            game_id=self.source.game_id,
             source_name=self.source.name,
             name=data["Name"],
             description=data["Description"],
             release_date=datetime.strptime(data["InitialReleaseDate"], DateFormat.SIMPLE).date(),
             repository=repository,
             plugin_file_name=data["PluginFile"],
-            version=fetch_latest_version(session, repository)
+            version=version
         )
         for author in parse_authors(data["Author"]):
             mod.authors.append(Authorship(mod=mod, name=author))
