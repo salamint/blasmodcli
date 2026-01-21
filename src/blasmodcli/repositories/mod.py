9bd83f62e3f82ce5a6a86adcc3275d228a321452
salamint <salamint@proton.me>
2026-01-21 20:08:50 +0100

    repositories: Added a method to search mods by terms in their name or description

--- src/blasmodcli/repositories/mod.py
+++ src/blasmodcli/repositories/mod.py
@@ -1,3 +1,7 @@
+from typing import Optional
+
+from sqlalchemy import or_
+
 from blasmodcli.model import ModSource, Mod, Game
 from blasmodcli.repositories.repository import Repository
 
@@ -23,3 +27,15 @@ class ModRepository(Repository):
                 source_name=source.name,
                 name=name
             ).one()
+
+    def search(self, game: Game, source: Optional[str], pattern: str) -> list[type[Mod]]:
+        with self.session() as session:
+            query = session.query(Mod).filter_by(
+                game_id=game.id
+            ).filter(or_(
+                Mod.name.ilike(pattern),
+                Mod.description.ilike(pattern)
+            ))
+            if source is not None:
+                query = query.filter_by(sourc_name=source)
+            return query.all()
