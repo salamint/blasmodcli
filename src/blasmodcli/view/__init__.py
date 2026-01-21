ea7e37cb3db748ff923e68c1d4fc5fa43b070dd4
salamint <salamint@proton.me>
2026-01-21 15:41:05 +0100

    view: Renamed 'Counter' to 'NumberedList', and created a new 'Counter' class

--- src/blasmodcli/view/__init__.py
+++ src/blasmodcli/view/__init__.py
@@ -1,6 +1,7 @@
 from .counter import Counter
 from .formatter import DateFormat, Formatter
 from .message import Message
+from .numbered_list import NumberedList
 from .progress import Progress
 from .table import Table
 from .update_list import UpdateList
