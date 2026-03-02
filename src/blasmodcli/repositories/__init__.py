aeb8662bac1a7a94386894158f856909cf69572e
salamint <salamint@proton.me>
2026-03-02 12:44:05 +0100

    repositories: The package now imports the content of both sub-packages instead of importing each individual repository

--- src/blasmodcli/repositories/__init__.py
+++ src/blasmodcli/repositories/__init__.py
@@ -1,5 +1,2 @@
-from .dependency import DependencyRepository
-from .game import GameRepository
-from .mod import ModRepository
-from .mod_source import ModSourceRepository
-from .warehouse import TableRepositories
+from .filesystems import *
+from .tables import *
