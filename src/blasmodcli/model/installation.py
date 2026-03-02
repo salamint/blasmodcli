5cd8adce23c022e42318db9513583f058c886617
salamint <salamint@proton.me>
2026-03-02 12:44:54 +0100

    model: The Installation now stores a ModVersion object, and has properties to access the mod and version independently

--- src/blasmodcli/model/installation.py
+++ src/blasmodcli/model/installation.py
@@ -1,17 +1,24 @@
 from datetime import datetime
 from pathlib import Path
 
-from blasmodcli.model.version import Version
-
 
 class Installation:
 
-    def __init__(self, file: Path, mod: Mod, version: Version, datetime: datetime):
+    def __init__(self, file: Path, mod_version: ModVersion):
         self.file = file
-        self.mod = mod
-        self.version = version
-        self.datetime = datetime
+        self.mod_version = mod_version
         self.files: list['File'] = []
+
+    @property
+    def mod(self) -> 'Mod':
+        return self.mod_version.mod
+
+    @property
+    def version(self) -> 'Version':
+        return self.mod_version.version
+
+    def get_datetime(self) -> datetime:
+        return datetime.fromtimestamp(self.file.stat().st_mtime)
 
     def persist(self):
         with self.file.open("w") as fd:
@@ -24,6 +31,12 @@ class Installation:
                 return True
         return False
 
+    def delete(self):
+        for file in self.files:
+            file.path.unlink(missing_ok=True)
+        self.file.unlink()
+
 
 from blasmodcli.model.file import File
-from blasmodcli.model.mod import Mod
+from blasmodcli.model.mod import Mod, ModVersion
+from blasmodcli.model.version import Version
