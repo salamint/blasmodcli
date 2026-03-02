8509a3fdca3930e8bf929fb09d79b14d780d3d47
salamint <salamint@proton.me>
2026-03-02 13:01:21 +0100

    cli: Improved the Context constructor and removed a useless attribute from the CommandHandler class and updated the Application class accordingly

--- src/blasmodcli/utils/cli/__init__.py
+++ src/blasmodcli/utils/cli/__init__.py
@@ -1,5 +1,6 @@
 from .argument import Argument
 from .choices import Choices
+from .context import CommandContext
 from .cli import CommandLineInterface
 from .handler import CommandHandler
 from .meta_handler import MetaCommandHandler
