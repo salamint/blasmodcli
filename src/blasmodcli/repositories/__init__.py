58a2d428125272a44a5e517c36359b5df0b255be
salamint <salamint@proton.me>
2026-04-05 12:07:11 +0200

    repositories: Renamed and removed installations

--- src/blasmodcli/repositories/__init__.py
+++ src/blasmodcli/repositories/__init__.py
@@ -1,6 +1,5 @@
 from .dependency import DependencyRepository
 from .game import GameRepository
 from .mod import ModRepository
-from .mod_installation import ModInstallationRepository
 from .mod_source import ModSourceRepository
-from .warehouse import Warehouse
+from .warehouse import TableRepositories
