cb4e1d9dd91c118f7ef83372ae3d73a6387e4fbe
salamint <salamint@proton.me>
2026-04-12 21:27:28 +0200

    config: Used the safer and intended `get` method instead of manually getting the value of fields in games configurations

--- src/blasmodcli/utils/config/games.py
+++ src/blasmodcli/utils/config/games.py
@@ -1,8 +1,12 @@
 from pathlib import Path
 
+from blasmodcli.exceptions.utils import MissingSectionException
 from blasmodcli.model import Game, ModdingTools, ModdingToolsDependency
 from blasmodcli.repositories import GameRepository
 from blasmodcli.utils.config.directory import ConfigurationDirectory
+
+
+MODDING_TOOLS_SECTION_NAME = "tools"
 
 
 class GameConfiguration(ConfigurationDirectory[Game]):
@@ -14,36 +18,36 @@ class GameConfiguration(ConfigurationDirectory[Game]):
 
     def load_data(self, data: dict) -> list[Game]:
         games = []
-        for key, attrs in data.items():
-            modding_tools = self.load_modding_tools(attrs["tools"])
+        for section, attrs in data.items():
+            modding_tools_section = f"{section}.{MODDING_TOOLS_SECTION_NAME}"
+            try:
+                modding_tools = self.load_modding_tools(modding_tools_section, attrs[MODDING_TOOLS_SECTION_NAME])
+            except KeyError:
+                raise MissingSectionException(self.current_file, modding_tools_section)
             game = Game(
-                id=key,
-                title=attrs["title"],
-                steamapp_id=attrs["steamapp_id"],
-                developer=attrs["developer"],
-                publisher=attrs["publisher"],
-                linux_native=attrs["linux_native"],
-                saves_directory=attrs["saves_directory"],
+                id=section,
+                title=self.get(attrs, section, "title", str),
+                steamapp_id=self.get(attrs, section, "steamapp_id", int),
+                developer=self.get(attrs, section, "developer", str),
+                publisher=self.get(attrs, section, "publisher", str),
+                linux_native=self.get(attrs, section, "linux_native", bool),
+                saves_directory=self.get(attrs, section, "saves_directory", str),
                 modding_tools=modding_tools
             )
             game = self.repository.update(game)
-            self.games[key] = game
+            self.games[section] = game
             games.append(game)
         return games
 
-    def load_modding_tools(self, data: dict) -> ModdingTools:
+    def load_modding_tools(self, section: str, data: dict) -> ModdingTools:
         modding_tools = ModdingTools(
-            mod_loader=data["mod_loader"],
-            format=data["format"],
-            url=data["url"],
-            author=data["author"],
+            mod_loader=self.get(data, section, "mod_loader", str),
+            format=self.get(data, section, "format", str),
+            url=self.get(data, section, "url", str),
+            author=self.get(data, section, "author", str),
             script_filename=data.get("script_filename"),
         )
         dependencies = data.pop("dependencies", {})
         for name, display_name in dependencies.items():
-            ModdingToolsDependency(
-                modding_tools=modding_tools,
-                name=name,
-                display_name=display_name
-            )
+            ModdingToolsDependency(modding_tools=modding_tools, name=name, display_name=display_name)
         return modding_tools
